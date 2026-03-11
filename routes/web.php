8ed5821e1ba62cb9256eac4e818a613b473401cd
Andrea030105 <tummoloandrea05@gmail.com>
2026-03-11 18:53:07 +0100

    yield-extendes-section-done

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,20 @@ Route::get('/', function () {
 
     return view('index', compact('comics', 'header_menu', 'footer_menu'));
 });
+
+Route::get('index/comic', function ($titolo) {
+
+    $header_menu = config('db.menu');
+    $footer_menu = config('db.footerMenu');
+
+    $comics = config('db.comics');
+    $titolo = 'Action Comics #1000: The Deluxe Edition';
+
+    foreach ($comics as $comic) {
+        if ($comic['title'] == $titolo) {
+            return $detail_comic = $comic;
+        }
+    }
+
+    return view('partials/comic', compact('comics', 'header_menu', 'footer_menu', 'detail_comic'));
+});

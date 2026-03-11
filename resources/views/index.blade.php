f5da2973bfe23c1ba00e9bf4e617ee017f7463ff
Andrea030105 <tummoloandrea05@gmail.com>
2026-03-11 11:20:55 +0100

    header-done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- Aggiungi questa riga ↓ --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
     <title>Laravel</title>
 
 </head>
--- resources/views/patrial/header.blade.php
+++ resources/views/patrial/header.blade.php
@@ -1,14 +1,32 @@
 <header>
-    <div class="container">
+    <div class="bg-primary banner-blu"></div>
+    <div class="container p-3">
         <div class="row">
-            <div class="col">
+            <div class="col-4">
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
             </div>
-            <div class="col">
-                @foreach($header_menu as $item)
-                {{$item['text']}}
-                @endforeach
+            <div class="col-8 d-flex align-items-center justify-content-center">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            @foreach($header_menu as $item)
+                            <li class="nav-item fw-bold text-uppercase">
+                                <a class="nav-link active" aria-current="page" href="#">{{$item['text']}}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+            </div>
+            </nav>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col p-0">
+                <div class="jumbotron">
+                </div>
             </div>
         </div>
+    </div>
     </div>
 </header>
